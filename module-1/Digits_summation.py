6fa59a3c2ae4e5210cf51a39e053eb5327fbcc4f
Tahmid <tahmid.dhrubo204256@gmail.com>
2026-03-16 16:13:10 +0600

    updated

--- module-1/Digits_summation.py
+++ module-1/Digits_summation.py
@@ -1,5 +1,14 @@
-x = int(input())
-y = int(input())
+inp = input()
 
-result  = (x%10) + (y%10)
-print(result)
+numbers = inp.split()
+
+x = int(numbers[0])  
+
+y = int(numbers[1])  
+
+last_digit_of_x = x % 10
+last_digit_of_y = y % 10
+
+sum = last_digit_of_x + last_digit_of_y
+
+print(sum)
